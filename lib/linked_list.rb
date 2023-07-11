1f8fce04a2e318c77464d8b088a4480a620976a7
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-07-10 21:20:02 -0400

    commit tree e1049ef29e7d344d9e8db865743bbc28546efa6a

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,14 +1,48 @@
 require_relative './node'
 
 class LinkedList
-  attr_accessor :head
+  attr_accessor :head, :count
+
 
   def initialize
     @head = nil
   end
 
   def nth_from_end(n)
-    # your code here
+    last_node = @head
+    count = 0 
+    i = 0
+
+    if n < 1
+      return nil
+    end
+
+    if last_node == nil
+      return nil
+    end
+
+    while last_node.next_node != nil
+      count = count + 1
+      last_node = last_node.next_node
+    end
+
+    count = count + 1
+
+    if n > count 
+      return nil
+    end
+
+    last_node = @head
+
+    while i < count - n 
+      last_node = last_node.next_node
+      i = i + 1
+    end
+
+    return last_node.value
+
+    #go through list to the count - n number 
+
   end
 
 end
